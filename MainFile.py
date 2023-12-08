89968c3614f88d148ed210be65c121a6c551e3f4
MarieF <124230375+MaireF597@users.noreply.github.com>
2023-12-08 14:19:26 -0600

    Default Password Finished

--- MainFile.py
+++ MainFile.py
@@ -17,10 +17,18 @@ if args.choice == "m":
         args.password = "d8578edf8458ce06fbc5bb76a58c5ca4"
     MD5(args.password)    
 elif args.choice == "s":
+    if(args.password == None):
+        args.password = "65e84be33532fb784c48129675f9eff3a682b27168c0ea744b2cf58ee02337c5"
     SHA256(args.password)
 elif args.choice == "D":
+    if(args.password == None):
+        args.password = "qwerty"
     DictionaryAttack(args.password)
 elif args.choice == "B":
+    if(agrs.password == None):
+        args.password = "qwerty"
     BruteForce(args.password)
 elif args.choice == "b":
+    if(args.password==None):
+        args.password = "$2a$12$D1035ZSwap0AEIddsJpKruxgGf4FuMzgdq2LxLEk.kF4Av25OlL7e"
     BCrypt(args.password)
